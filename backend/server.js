da66eb2d0708b925a6834c283a09d64b050c3a26
archit203 <archit241001@gmail.com>
2024-03-10 17:22:04 +0530

    port

--- backend/server.js
+++ backend/server.js
@@ -99,8 +99,9 @@ app.delete("/transactions/:id", authenticator, async (req,res) => {
     }
 })
 
-app.listen(3000, () => {
-    console.log("Server started on http://localhost:3000");
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`Server started on http://localhost:${port}`);
 })
 
 
